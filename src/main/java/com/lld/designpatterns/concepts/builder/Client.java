aa180c52d2ac2b998214ae3317083c84089fd95d
Priyanka-Ravichandran-P <priya.r6907@gmail.com>
2023-04-24 22:05:23 +0530

    builder class code clean

--- src/main/java/com/lld/designpatterns/concepts/builder/Client.java
+++ src/main/java/com/lld/designpatterns/concepts/builder/Client.java
@@ -3,27 +3,21 @@ package com.lld.designpatterns.concepts.builder;
 import com.lld.designpatterns.concepts.builder.Student.Builder;
 
 public class Client {
-/**
- *  Requirement:
- *  Student Object should not get create before validation,
- *  After the validation check passed then only student object should get created
- */
-	public static void main(String[] args) throws Exception{
+	/**
+	 * Requirement: Student Object should not get create before validation, After
+	 * the validation check passed then only student object should get created
+	 */
+	public static void main(String[] args) throws Exception {
 		try {
-			Builder builder = Student.builder();
-			Student student = builder.setAge(25)
-						.setId(2)
-							.setName("Priyanka")
-								.setPhoneNumber(963258741)
-									.setPsp(95).build();
-			
+			Student student = Student.builder().setAge(25)
+					.setId(2).setName("Priyanka").setPhoneNumber(963258741).setPsp(95).build();
+
 			System.out.print("STUDENT :: " + student.toString());
-		}catch(Exception e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 			throw new Exception(e);
-			
+
 		}
-		
 
 	}
 }
